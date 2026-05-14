231c109070dcb1d2382068da8c0f5c9bc939e7f4
nnikcho-5m <nikolaypgee2@gmail.com>
2026-05-14 19:18:00 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_fxv2_oal 'yes'
+
+author 'NN TEAM'
+description 'Advanced Fivem HUD System'
+version '1.0.0'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/app.js'
+}   
+
+client_script 'client/client.lua'
+shared_script {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
